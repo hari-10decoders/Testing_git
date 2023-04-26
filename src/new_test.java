ba229e867a34dee4bec2d1865c210949643725ca
hari-10decoders <hariprasad.m@10decoders.in>
2023-04-26 13:16:58 +0530

    newcode

--- src/new_test.java
+++ src/new_test.java
@@ -1,8 +1,21 @@
 
 public class new_test {
+	
+	public void Car() {
+		System.out.println("price");
+	}
+	
+	public void honda() {
+		System.out.println("10000");
+	}
+	
+	public void tata() {
+		System.out.println("100500");
+	}
 public static void main(String[] args) {
 	System.out.println("Click the Button");
 	System.out.println("newadd");
+	
 
 }
 }
