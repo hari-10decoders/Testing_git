65337c7b3e1df2698bbdb2d059801c0daa512780
hari-10decoders <hariprasad.m@10decoders.in>
2023-04-26 17:30:46 +0530

    newcodefix

--- src/new_test.java
+++ src/new_test.java
@@ -1,20 +1,22 @@
 
 public class new_test {
 	
-	public void Car() {
+	public void bike() {
 		System.out.println("price");
 	}
 	
-	public void honda() {
+	public void pulsar() {
 		System.out.println("10000");
 	}
 	
-	public void tata() {
+	public void honda() {
 		System.out.println("100500");
 	}
 public static void main(String[] args) {
 	System.out.println("Click the Button");
 	System.out.println("newadd");
+	
+	
 	
 
 }
